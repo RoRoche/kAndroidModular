701bea90ee03c830cc837880af9798a035d2d216
Romain Rochegude <romain.rochegude@gmail.com>
2018-01-08 22:58:29 +0100

    declare and build the global FSM

--- kAndroidModular/app/src/main/java/fr/guddy/kandroidmodular/usecases/MainActivity.kt
+++ kAndroidModular/app/src/main/java/fr/guddy/kandroidmodular/usecases/MainActivity.kt
@@ -1,16 +1,28 @@
 package fr.guddy.kandroidmodular.usecases
 
+import android.arch.lifecycle.Observer
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.github.rossdanderson.stateless4k.StateMachine
+import com.github.rossdanderson.stateless4k.config.StateMachineConfiguration
 import fr.guddy.kandroidmodular.R
+import fr.guddy.kandroidmodular.fsm.FsmViewModel
+import fr.guddy.kandroidmodular.fsm.Model
 import fr.guddy.kandroidmodular.usecases.userinput.UserInputFragment
 import fr.guddy.kandroidmodular.usecases.userrepos.UserReposFragment
+import org.koin.android.architecture.ext.getViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: FsmViewModel
+    private lateinit var stateMachine: StateMachine<State, Trigger, Model>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewModel = getViewModel()
+        startStateMachine()
 
         showUserInputFragment()
     }
@@ -24,6 +36,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun showUserReposFragment(user: String) {
+        stateMachine.fire(Trigger.OnUserFilled(user))
         supportFragmentManager.beginTransaction()
                 .replace(R.id.ActivityMain_ViewGroup_Container, UserReposFragment.newInstance(user))
                 .addToBackStack(null)
@@ -32,9 +45,51 @@ class MainActivity : AppCompatActivity() {
 
     override fun onBackPressed() {
         if (supportFragmentManager.backStackEntryCount > 0) {
+            stateMachine.fire(Trigger.OnBackPressed())
             supportFragmentManager.popBackStack()
         } else {
             super.onBackPressed()
         }
+    }
+
+    fun startStateMachine() {
+        val stateMachineModel = StateMachineConfiguration.create<State, Trigger, Model> {
+            state(State.WaitingUserInput) {
+                onEntry(Model::onEntryState)
+                onExit(Model::onExitState)
+                onOneOf(Trigger.OnUserFilled::class.java) {
+                    transitionTo(State.ShowingUserRepos) {
+                        onEntry(Model::onEntryState)
+                        onExit(Model::onExitState)
+                        onOneOf(Trigger.OnBackPressed::class.java) {
+                            transitionTo(State.WaitingUserInput)
+                        }
+                    }
+                }
+            }
+        }.build()
+        stateMachine = stateMachineModel.start(Model(), State.ShowingUserRepos)
+
+        viewModel.context.observe(this, Observer {
+            it?.let { context ->
+                stateMachine = stateMachineModel.start(
+                        context.model,
+                        context.model.currentState!!,
+                        context.performEntryActionsForInitialState
+                )
+            }
+        })
+    }
+
+    enum class State {
+        WaitingUserInput, ShowingUserRepos
+    }
+
+    /**
+     * The classes we're going to use instances of for our triggers.
+     */
+    sealed class Trigger {
+        class OnUserFilled(val user: String) : Trigger()
+        class OnBackPressed : Trigger()
     }
 }
